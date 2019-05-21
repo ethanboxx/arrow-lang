8d7b6a3fcad5741bd20e445ed108e8eee8999e59
Ethan Brierley <ethanboxx@gmail.com>
2019-05-21 11:35:12 +0100

    gitlab

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE ViewPatterns #-}
-
 module Main where
 
 import           Lib
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE ViewPatterns #-}
-
 module Lib
     ( tokenizeSentence
     )
