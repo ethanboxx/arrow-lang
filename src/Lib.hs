236eb3206f0303cc30f1fe4dd24b4c4abd56766a
Ethan Brierley <ethanboxx@gmail.com>
2019-05-22 14:20:45 +0100

    redo work

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,39 +1,21 @@
 module Lib
-    ( tokenizeSentence
-    , tokenizeWord
+    ( 
+     tokenizeWord
     )
 where
 
-import qualified Data.Text.Lazy                as T
-
-type Word = T.Text
+import qualified Data.Text.Lazy             as T
 
 
-data Token = Word | ArrowLeft | ArrowRight | If deriving( Show )
 
-tokenizeSentence :: T.Text -> [Token]
-tokenizeSentence sent = case nextToken of
-    Just If         -> If : (tokenizeSentence (T.drop 3 sent))
-    Just ArrowLeft  -> ArrowLeft : (tokenizeSentence (T.drop 3 sent))
-    Just ArrowRight -> ArrowRight : (tokenizeSentence (T.drop 3 sent))
-    Nothing         -> []
-    where nextToken = tokenizeWord sent
-
-tokenizeWord :: T.Text -> Maybe Token
-tokenizeWord x = if (T.length x) == 0
-    then Nothing
-    else if (T.head x == ' ') || (T.head x == '\n')
-        then tokenizeWord (T.tail x)
-        else if isNextWord "if"
-            then Just If
-            else if isNextWord "<-"
-                then Just ArrowLeft
-                else if isNextWord "->" then Just ArrowRight else Nothing
-    where isNextWord = isNextWordOf x
+data WordWithEnd =  WordWithEnd {word::T.Text, endSymbol:: T.Text }
 
 
-isNextWordOf :: T.Text -> T.Text -> Bool
-isNextWordOf sentence word = (T.snoc word ' ') == firstLengthLetters
-  where
-    wordLength         = T.length word
-    firstLengthLetters = T.take (wordLength + 1) sentence
+tokenizeWord :: T.Text -> [T.Text]
+tokenizeWord x =   moveOut  "==" "if (T.length x) == 0"
+
+addInside :: T.Text -> [T.Text] -> [T.Text]
+addInside with l@(x:xs) = x:(if length l /= 1 then (with:(addInside with xs))else addInside with xs)
+addInside with [] = []
+
+moveOut x t =  ( addInside x . T.splitOn x) t
