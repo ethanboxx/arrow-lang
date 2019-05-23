17c6bbf1f79d8c8e9910121b6fb2a9af05dc2e51
Ethan Brierley <ethanboxx@gmail.com>
2019-05-23 16:02:42 +0100

    hlint

--- src/Lib.hs
+++ src/Lib.hs
@@ -16,7 +16,7 @@ data Expr = Func {_funcName :: T.Text}  deriving (Show)
 toMLRInstruction :: [Token] -> Either String Instruction
 toMLRInstruction (Word x : VarAssignmentT : xs) = do
   expr <- toMLRExpr xs
-  Right (VarAssignmentI (VarAssignment { _varName = x, _expr = expr }))
+  Right (VarAssignmentI VarAssignment { _varName = x, _expr = expr })
 
 toMLRExpr :: [Token] -> Either String Expr
 toMLRExpr (Word x : xs) = Right Func { _funcName = x }
