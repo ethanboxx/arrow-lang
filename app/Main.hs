29b2dac3e407fd270d839592f3630abcc30f3a2d
Ethan Brierley <ethanboxx@gmail.com>
2019-05-23 11:50:19 +0100

    use do

--- app/Main.hs
+++ app/Main.hs
@@ -5,4 +5,6 @@ import           Lib
 test = "thing <- 10 * 4"
 
 main :: IO ()
-main = print (tokenize test) >> print ((toMLRInstruction . tokenize) test)
+main = do
+    print $ tokenize test
+    print $ (toMLRInstruction . tokenize) test
